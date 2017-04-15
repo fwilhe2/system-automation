786371707da047f4244e7b0ce0bacfa099caf2d0
Florian Wilhelm <florian-wilhelm@onlinehome.de>
2017-04-15 15:41:42 +0200

    Simplify run.sh script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo ansible-galaxy install --force -r requirements.yml
-ansible-playbook -K -i inventory common.yml
-ansible-playbook -K -i inventory software-development.yml
+ansible-playbook --inventory=inventory common.yml
+ansible-playbook --inventory=inventory software-development.yml
