2d6d12e57a9498d78ee36d74e1d6b32e584bf7af
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-01-03 13:14:53 +0100

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,5 @@ set -o errexit
 
 tmp_dir=$(mktemp -d -t sysauto-XXXXXXXXXX)
 curl --location https://github.com/fwilhe2/system-automation/archive/master.zip -o ${tmp_dir}/sysauto.zip
-unzip /tmp/sysauto.zip -d ${tmp_dir}/extract
+unzip ${tmp_dir}/sysauto.zip -d ${tmp_dir}/extract
 ansible-playbook --ask-become-pass --inventory inventory ${tmp_dir}/extract/system-automation-master/common.yml
