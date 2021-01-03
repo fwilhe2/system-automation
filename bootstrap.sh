b2b0791b70b40fdaf96bd628a4654b81dea5b398
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-01-03 13:11:22 +0100

    bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+tmp_dir=$(mktemp -d -t sysauto-XXXXXXXXXX)
+curl --location https://github.com/fwilhe2/system-automation/archive/master.zip -o ${tmp_dir}/sysauto.zip
+unzip /tmp/sysauto.zip -d ${tmp_dir}/extract
+ansible-playbook --ask-become-pass --inventory inventory ${tmp_dir}/extract/system-automation-master/common.yml
