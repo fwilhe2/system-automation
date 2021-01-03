1958910fda3810f0b6a90da06fea75a247f1f424
Florian Wilhelm <52838694+fwilhe2@users.noreply.github.com>
2021-01-03 13:34:01 +0100

    Add Bootstrap script (#13)

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+tmp_dir=$(mktemp -d -t sysauto-XXXXXXXXXX)
+curl --location https://github.com/fwilhe2/system-automation/archive/master.zip -o ${tmp_dir}/sysauto.zip
+unzip ${tmp_dir}/sysauto.zip -d ${tmp_dir}/extract
+ansible-playbook --ask-become-pass --inventory ${tmp_dir}/extract/system-automation-master/inventory ${tmp_dir}/extract/system-automation-master/common.yml
