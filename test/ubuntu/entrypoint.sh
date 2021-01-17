5db56e8598e98756ebe35d5e1ba3a11f5527af65
Florian Wilhelm <52838694+fwilhe2@users.noreply.github.com>
2021-01-17 13:05:57 +0100

    Use custom container image for testing playbook (#17)
    Hope this will be more reliable compared to the old method

---
+++ test/ubuntu/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+
+set -x
+
+echo "::group::Run Playbook"
+ansible-playbook -vv /mnt/common.yml
+source ~/.custom-path.sh # Make sure path is updated
+echo "::endgroup::"
+
+echo "::group::Test Installed Commands"
+declare -a EXPECTED_COMMANDS=("java" "mvn" "gradle" "go" "kotlinc" "node")
+for i in "${EXPECTED_COMMANDS[@]}"
+do
+    if ! [ -x "$(command -v $i)" ]; then
+        echo "Error: Expected $i to be installed." >&2
+        exit 1
+    fi
+done
+echo "::endgroup::"
