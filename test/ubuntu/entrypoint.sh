d7a640456183c54fa14bbe7ce997c98c65be0d73
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-01-17 12:51:00 +0100

    test

--- test/ubuntu/entrypoint.sh
+++ test/ubuntu/entrypoint.sh
@@ -1,8 +1,17 @@
 #!/bin/bash -l
 
+echo "::group::Run Playbook"
 ansible-playbook -vv /mnt/common.yml
+source ~/.custom-path.sh # Make sure path is updated
+echo "::endgroup::"
 
-source ~/.custom-path.sh
-
-java -version
-mvn --version
+echo "::group::Test Installed Commands"
+declare -a EXPECTED_COMMANDS=("java" "mvn" "gradle" "go" "kotlinc" "node")
+for i in "${arr[@]}"
+do
+    if ! [ -x "$(command -v $i)" ]; then
+        echo "Error: Expected $i to be installed." >&2
+        exit 1
+    fi
+done
+echo "::endgroup::"
