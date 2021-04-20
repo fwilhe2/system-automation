2b59f04078b48d067f099c9d27b8b7b183ed1161
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-04-20 22:15:08 +0200

    test

--- test/container/entrypoint.sh
+++ test/container/entrypoint.sh
@@ -5,6 +5,8 @@ set -o errexit
 
 cat /etc/os-release
 
+id
+
 echo "::group::Install rust (requirement of dotfiles)"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
@@ -27,7 +29,7 @@ echo "::endgroup::"
 # Idempotence check, via https://github.com/geerlingguy/mac-dev-playbook/blob/7382e0241fe27cf17fabe31582af0269551e7004/.github/workflows/ci.yml#L71
 echo "::group::Idempotence check"
 idempotence=$(mktemp)
-ansible-playbook --become --become-method=su /mnt/common.yml | tee -a ${idempotence}
+ansible-playbook --become-method=su /mnt/common.yml | tee -a ${idempotence}
 tail ${idempotence} | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
 echo "::endgroup::"
 
@@ -43,6 +45,6 @@ echo "::endgroup::"
 
 echo "::group::Idempotence check"
 idempotence=$(mktemp)
-ansible-playbook --become --become-method=su /mnt/desktop.yml | tee -a ${idempotence}
+ansible-playbook --become-method=su /mnt/desktop.yml | tee -a ${idempotence}
 tail ${idempotence} | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
 echo "::endgroup::"
