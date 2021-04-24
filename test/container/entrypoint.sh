32502c22f7cdd239009f09d96fc08ec8e9a4f083
Florian Wilhelm <52838694+fwilhe2@users.noreply.github.com>
2021-04-24 11:07:24 +0200

    Install Codium Plugins (#29)

--- test/container/entrypoint.sh
+++ test/container/entrypoint.sh
@@ -5,13 +5,15 @@ set -o errexit
 
 cat /etc/os-release
 
+id
+
 echo "::group::Install rust (requirement of dotfiles)"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 echo "::endgroup::"
 
 echo "::group::Run Playbook"
-ansible-playbook -vv /mnt/common.yml
+ansible-playbook --skip-tags notest -vv /mnt/common.yml
 source ~/.custom-path.sh # Make sure path is updated
 source ~/.bashrc
 echo "::endgroup::"
@@ -27,21 +29,22 @@ echo "::endgroup::"
 # Idempotence check, via https://github.com/geerlingguy/mac-dev-playbook/blob/7382e0241fe27cf17fabe31582af0269551e7004/.github/workflows/ci.yml#L71
 echo "::group::Idempotence check"
 idempotence=$(mktemp)
-ansible-playbook /mnt/common.yml | tee -a ${idempotence}
+ansible-playbook --skip-tags notest /mnt/common.yml | tee -a ${idempotence}
 tail ${idempotence} | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
 echo "::endgroup::"
 
 echo "::group::Run Desktop Playbook"
-ansible-playbook -vv /mnt/desktop.yml
+ansible-playbook --skip-tags notest -vv /mnt/desktop.yml
 echo "::endgroup::"
 
 echo "::group::Check Desktop App Versions"
 codium --user-data-dir=/tmp --version
+codium --user-data-dir=/tmp --list-extensions
 keepassxc-cli --version
 echo "::endgroup::"
 
 echo "::group::Idempotence check"
 idempotence=$(mktemp)
-ansible-playbook /mnt/desktop.yml | tee -a ${idempotence}
+ansible-playbook --skip-tags notest /mnt/desktop.yml | tee -a ${idempotence}
 tail ${idempotence} | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
 echo "::endgroup::"
