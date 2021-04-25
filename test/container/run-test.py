67d216e8fea4e8f8956e8ef75dffea68608fc3d4
Florian Wilhelm <52838694+fwilhe2@users.noreply.github.com>
2021-04-25 10:02:46 +0200

    Update run-test.py

--- test/container/run-test.py
+++ test/container/run-test.py
@@ -1,4 +1,4 @@
 import subprocess
 
 subprocess.run(["bash", "-c", "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"])
-assert subprocess.run(["ansible-playbook", "--skip-tags", "notest", "-vv", "/home/user/common.yml"]).returncode == 0
+assert subprocess.run(["ansible-playbook", "--become-method=su", "--skip-tags", "notest", "-vv", "/home/user/common.yml"]).returncode == 0
