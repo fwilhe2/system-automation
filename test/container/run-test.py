dabdd69817840b6c54592155a800d30a98847f2a
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-04-25 11:04:00 +0200

    fn

--- test/container/run-test.py
+++ test/container/run-test.py
@@ -3,6 +3,14 @@ import shutil
 import os
 from pathlib import Path
 
+def run_ansible(playbook):
+    assert (
+        subprocess.run(
+            ["ansible-playbook", "--skip-tags", "notest", "-vv", playbook]
+        ).returncode
+        == 0
+    )
+
 subprocess.run(
     [
         "bash",
@@ -10,24 +18,10 @@ subprocess.run(
         "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y",
     ]
 )
-assert (
-    subprocess.run(
-        ["ansible-playbook", "--skip-tags", "notest", "-vv", "/mnt/common.yml"]
-    ).returncode
-    == 0
-)
 
-paths = ['apache-maven-3.8.1', 'gradle-7.0', 'sapmachine-jdk-11.0.10']
-
-for path in paths:
-    p = Path.home() / 'software' / path
-    assert p.exists()
-    assert p.is_dir()
-
-
-print(os.environ['PATH'])
-
-# assert shutil.which("mvn") is not None
-# assert shutil.which("javac") is not None
-# assert shutil.which("gradle") is not None
-# assert shutil.which("kotlinc") is not None
+run_ansible("/mnt/common.yml")
+run_ansible("/mnt/common.yml")
+run_ansible("/mnt/common.yml")
+run_ansible("/mnt/desktop.yml")
+run_ansible("/mnt/desktop.yml")
+run_ansible("/mnt/desktop.yml")
