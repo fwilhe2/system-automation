dc2e1ed569214c747d97bd977f031cecc689fab3
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-04-25 12:17:37 +0200

    update

--- test/container/run-test.py
+++ test/container/run-test.py
@@ -4,6 +4,7 @@ import os
 from pathlib import Path
 import re
 
+
 def run_ansible(playbook):
     print(f"::group::Running Playbook {playbook}")
     assert (
@@ -12,20 +13,19 @@ def run_ansible(playbook):
         ).returncode
         == 0
     )
-    # Idempotence check
+    # Idempotence check: Run again and verify nothing fails or changes the second time
+    # Idea via https://github.com/geerlingguy/mac-dev-playbook/blob/7382e0241fe27cf17fabe31582af0269551e7004/.github/workflows/ci.yml#L71
     rerun = subprocess.run(
-        ["ansible-playbook", "--skip-tags", "notest", "-vv", playbook], capture_output=True
+        ["ansible-playbook", "--skip-tags", "notest", playbook], capture_output=True
     )
-    assert(rerun.returncode == 0)
-
-    # debug
-    print(rerun.stdout.decode("utf-8"))
-
-    dbg = re.fullmatch(".*changed=0.*", rerun.stdout.decode("utf-8"), re.DOTALL)
-    print(dbg)
-
-    assert(dbg)
+    assert rerun.returncode == 0
+    changed_match = re.fullmatch(
+        ".*changed=0.*failed=0.*", rerun.stdout.decode("utf-8"), re.DOTALL
+    )
+    print(changed_match)
+    assert changed_match
     print("::endgroup::")
+
 
 print("::group::Install Rust")
 subprocess.run(
