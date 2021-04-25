8de37b98574cc628a85b636b1a1bbab7edd624ac
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-04-25 09:39:19 +0200

    assert

--- test/container/run-test.py
+++ test/container/run-test.py
@@ -4,5 +4,7 @@ import shutil
 subprocess.run(["bash", "-c", "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"])
 subprocess.run(["ansible-playbook", "--skip-tags", "notest", "-vv", "/mnt/common.yml"])
 
-shutil.which('javac')
-shutil.which('mvn')
+assert shutil.which('mvn') != None
+assert shutil.which('javac') != None
+assert shutil.which('gradle') != None
+assert shutil.which('kotlinc') != None
