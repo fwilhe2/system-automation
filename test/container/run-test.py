89c031b9fea1a4c665db1ab6770e52eb1b1debdc
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-04-25 11:22:14 +0200

    group msg

--- test/container/run-test.py
+++ test/container/run-test.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import re
 
 def run_ansible(playbook):
+    print(f"::group::Running Playbook {playbook}")
     assert (
         subprocess.run(
             ["ansible-playbook", "--skip-tags", "notest", "-vv", playbook]
@@ -17,7 +18,9 @@ def run_ansible(playbook):
     )
     assert(rerun.returncode == 0)
     assert(re.match("changed=0.*failed=0", rerun.stdout.decode("utf-8")))
+    print("::endgroup::")
 
+print("::group::Install Rust")
 subprocess.run(
     [
         "bash",
@@ -25,6 +28,7 @@ subprocess.run(
         "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y",
     ]
 )
+print("::endgroup::")
 
 run_ansible("/mnt/common.yml")
 run_ansible("/mnt/desktop.yml")
