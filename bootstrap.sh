267cf4bcd0910df4b7e77a44be6180b48d5dc38e
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-05-13 22:31:13 +0200

    Improve bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,12 @@
 set -o nounset
 set -o errexit
 
+if [ -e "/usr/bin/apt-get" ] ; then
+    sudo -y apt-get update && sudo -y apt-get upgrade && sudo apt-get -y install ansible curl bash unzip
+elif [ -e "/usr/bin/dnf" ] ; then
+    sudo dnf -y upgrade && sudo dnf install -y ansible curl bash unzip
+fi
+
 tmp_dir=$(mktemp -d -t sysauto-XXXXXXXXXX)
 curl --location https://github.com/fwilhe2/system-automation/archive/master.zip -o ${tmp_dir}/sysauto.zip
 unzip ${tmp_dir}/sysauto.zip -d ${tmp_dir}/extract
