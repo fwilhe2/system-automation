d076e7caf82e0b92fa93a237489332795d0ec7fa
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-06-16 22:43:41 +0200

    update

--- update-versions.py
+++ update-versions.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python
 
 import yaml
+import json
+import urllib.request
+import urllib.parse
 
-# todo: this script should be able to get the latest versions and update them automated
-with open("roles/bin/defaults/main.yml", 'r') as stream:
-    try:
-        versions = yaml.safe_load(stream)
-        for v in versions:
-            print(v)
-            print(versions[v])
-    except yaml.YAMLError as exc:
-        print(exc)
+# Fixme: non-trivial cases commented out for now
+repos = {
+    # "SAP/SapMachine": "JDK_VERSION",
+    # "apache/maven": "MAVEN_VERSION",
+    "gradle/gradle": "GRADLE_VERSION",
+    # "apache/ant": "ANT_VERSION",
+    "nodejs/node": "NODE_VERSION",
+    # "golang/go": "GO_VERSION",
+    "cli/cli": "GH_VERSION",
+    "JetBrains/kotlin": "KOTLIN_VERSION",
+    # "r-darwish/topgrade": "TOPGRADE_VERSION",
+}
+
+# current_versions = {'GRADLE_VERSION': '7.1.0', 'NODE_VERSION': '14.17.1', 'GH_VERSION': '1.11.0', 'KOTLIN_VERSION': '1.5.10'}
+current_versions = {}
+
+for r in repos:
+    print(r)
+    url = f"https://api.github.com/repos/{r}/releases/latest"
+    f = urllib.request.urlopen(url)
+    tag_name = json.loads(f.read().decode("utf-8"))["tag_name"]
+    current_versions[repos.get(r)] = tag_name.removeprefix('v')
+
+print(current_versions)
