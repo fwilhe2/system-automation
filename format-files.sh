b51f775ec435a540ed1461e742ff81342dfbb738
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-07-18 17:55:31 +0200

    Update

--- format-files.sh
+++ format-files.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -o nounset
 set -o errexit
-set -o xtrace   
+set -o xtrace
 
 npx prettier --write "./**/*.yml"
-command -v yapf3 >/dev/null 2>&1 && alias yapf=yapf3
-yapf --recursive --in-place .
+
+command -v yapf3 >/dev/null 2>&1 && yapf3 --recursive --in-place .
+command -v yapf >/dev/null 2>&1 && yapf --recursive --in-place .
