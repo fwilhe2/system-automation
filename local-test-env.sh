bc4efe0c213eb03828e35771089d597474bc06e0
Florian Wilhelm <52838694+fwilhe2@users.noreply.github.com>
2022-01-28 12:41:45 +0100

    Allow running on Archlinux (#66)

--- local-test-env.sh
+++ local-test-env.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -o nounset
 set -o errexit
+set -x
 
 DISTRO=fedora
-
-if [[ "$#" -eq 1 ]]; then
+if [[ "$#" -ge 1 ]]; then
     DISTRO=$1
+fi
+
+ENTRYPOINT=''
+if [[ "$#" -ge 2 ]]; then
+    ENTRYPOINT="--entrypoint=$2"
 fi
 
 CLI=docker
@@ -13,4 +18,4 @@ CLI=docker
 command -v podman >/dev/null 2>&1 && CLI=podman
 
 $CLI build --build-arg=VERSION=latest -t system-automation-$DISTRO --file test/container/Containerfile.$DISTRO .
-$CLI run -it --rm --volume $PWD:/mnt --privileged system-automation-$DISTRO
+$CLI run -it --rm --volume $PWD:/mnt $ENTRYPOINT --privileged system-automation-$DISTRO
