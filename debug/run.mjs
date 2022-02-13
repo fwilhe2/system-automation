8f23fabc13d20bc3feafdddb0c8ccccd7644a8ad
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2022-02-13 18:46:23 +0100

    improve

--- debug/run.mjs
+++ debug/run.mjs
@@ -2,7 +2,8 @@
 
 const matrix = {
     'debian': ['latest', 'testing', 'unstable'],
-    'ubuntu': ['latest', 'rolling', 'devel']
+    'ubuntu': ['latest', 'rolling', 'devel'],
+    'fedora': ['latest', 'rawhide']
 }
 
 const distros = Object.keys(matrix)
@@ -11,7 +12,7 @@ const images = distros.flatMap(d => matrix[d].map(t => `${d}:${t}`))
 console.log(images)
 const pulls = images.map(i => $`docker pull ${i}`)
 await Promise.all(pulls)
-const builds = distros.flatMap(distro => matrix[distro].map(tag => $`docker build --build-arg=DISTRO=${distro} --build-arg=VERSION=${tag} --tag=debug-${distro}:${tag} .`))
+const builds = distros.flatMap(distro => matrix[distro].map(tag => $`docker build --file=Dockerfile.${distro} --build-arg=VERSION=${tag} --tag=debug-${distro}:${tag} .`))
 await Promise.all(builds)
 const runs = images.map(i => $`docker run -t --rm -v $PWD:/debug debug-${i}`)
 await Promise.all(runs)
