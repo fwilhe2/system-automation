ea8a63a7eed9d6244dc6ba714f89c3760e563031
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2022-02-13 19:16:34 +0100

    update

--- debug/run.mjs
+++ debug/run.mjs
@@ -8,11 +8,14 @@ const matrix = {
 
 const distros = Object.keys(matrix)
 
-const images = distros.flatMap(d => matrix[d].map(t => `${d}:${t}`))
+const images = distros.flatMap(distro => matrix[distro].map(tag => `${distro}:${tag}`))
 console.log(images)
-const pulls = images.map(i => $`docker pull ${i}`)
+
+const pulls = images.map(image => $`docker pull ${image}`)
 await Promise.all(pulls)
+
 const builds = distros.flatMap(distro => matrix[distro].map(tag => $`docker build --file=Dockerfile.${distro} --build-arg=VERSION=${tag} --tag=debug-${distro}:${tag} .`))
 await Promise.all(builds)
-const runs = images.map(i => $`docker run -t --rm -v $PWD:/debug debug-${i}`)
+
+const runs = images.map(image => $`docker run -t --rm -v $PWD:/debug debug-${image}`)
 await Promise.all(runs)
