d9530506f762e7940dc2bd42e1bf58a88bb5ffee
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2022-02-13 10:10:31 +0100

    sync run

--- debug/run.js
+++ debug/run.js
@@ -1,44 +1,38 @@
-const { spawn } = require('child_process');
+const { execSync } = require('child_process');
 
 const matrix = {
     'debian': ['latest', 'testing', 'unstable'],
     'ubuntu': ['latest', 'rolling', 'devel']
 }
 
-function runCommand(command, args) {
-    console.log(`> ${command} ${args.toString()}`)
-    const cmd = spawn(command, args)
-
-
-    cmd.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-    });
-
-    cmd.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-    });
-
-    cmd.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-    });
+function runCommand(command) {
+    console.log(`> ${command}`)
+    console.log(execSync(command).toString())
 }
 
 Object.keys(matrix).forEach(distro => {
     const tags = matrix[distro]
 
+    console.log(`DISTRO: ${distro}`)
+    tags.forEach(tag => {
+        console.log(`TAG   : ${tag}`)
+    })
+
     tags.forEach(tag => {
         console.log(`=== PULL  ${distro}:${tag}`)
-        runCommand('docker', ['pull', `${distro}:${tag}`])
+        // runCommand('docker', ['pull', `${distro}:${tag}`])
+        runCommand(`docker pull ${distro}:${tag}`)
     })
 
     tags.forEach(tag => {
         console.log(`=== BUILD  ${distro}:${tag}`)
-        runCommand('docker', ['build', `--build-arg=DISTRO=${distro}`, `--build-arg=VERSION=${tag}`, `--tag=debug-${distro}:${tag}`, '.'])
-
+        // runCommand('docker', ['build', `--build-arg=DISTRO=${distro}`, `--build-arg=VERSION=${tag}`, `--tag=debug-${distro}:${tag}`, '.'])
+        runCommand(`docker build --build-arg=DISTRO=${distro} --build-arg=VERSION=${tag} --tag=debug-${distro}:${tag} .`)
     })
 
     tags.forEach(tag => {
         console.log(`=== RUN  ${distro}:${tag}`)
-        runCommand('docker', ['run', '-t', '--rm', '-v', '$PWD:/debug', `debug-${distro}:${tag}`])
+        // runCommand('docker', ['run', '-t', '--rm', '-v', '$PWD:/debug', `debug-${distro}:${tag}`])
+        runCommand(`docker run -t --rm -v $PWD:/debug debug-${distro}:${tag}`)
     })
 })
