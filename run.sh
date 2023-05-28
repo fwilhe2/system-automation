5ee17760589e9717f111b19375c510b2e336ee2d
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2023-05-28 10:07:51 +0200

    Refactor dev tooling in separate playbook

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 set -o nounset
 set -o errexit
 
-# Common playbook is default
-PLAYBOOK="${1:-common}"
+# Dev playbook is default
+PLAYBOOK="${1:-dev}"
 
 ansible-playbook --ask-become-pass --inventory inventory $PLAYBOOK.yml
