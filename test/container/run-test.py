4c2fa3f4f97b0e5d4cb60bfc897809bd207fe5cf
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2023-07-16 16:34:27 +0200

    fix

--- test/container/run-test.py
+++ test/container/run-test.py
@@ -88,6 +88,12 @@ def ansible_playbook_executable():
         return "/home/user/.local/bin/ansible-playbook"
     return in_path
 
+def ansible_galaxy_executable():
+    in_path = shutil.which("ansible-galaxy")
+    if in_path == None:
+        return "/home/user/.local/bin/ansible-galaxy"
+    return in_path
+
 def print_os_version():
     print(distro.name(pretty=True))
 
