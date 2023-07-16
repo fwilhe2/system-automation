da9e35aad275e8cf2b4b2163400d8539a8d4552d
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2023-07-16 13:07:47 +0200

    test

--- test/container/Containerfile.el
+++ test/container/Containerfile.el
@@ -4,7 +4,7 @@ FROM $VERSION
 
 ENV TERM=xterm
 
-RUN dnf --assumeyes update && dnf --assumeyes --allowerasing install ansible python3 python3-pip python3-distro git unzip bash coreutils curl \
+RUN dnf --assumeyes update && dnf --assumeyes --allowerasing install python3 python3-pip python3-distro git unzip bash coreutils curl \
   && mkdir -p /home/user && echo "user:x:1001:1001:user:/home/user:/bin/bash" >> /etc/passwd \
   && chown -R user /home/user
 
@@ -15,7 +15,11 @@ RUN echo -e '[local]\nlocalhost ansible_connection=local ansible_become_password
 
 RUN echo 'root:toor123' | chpasswd
 
+RUN python3 -m pip install --upgrade pip
+
 USER user
+
+RUN python3 -m pip install --user ansible
 
 COPY . /home/user
 
