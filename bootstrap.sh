be079bb56f15b93c7d64d18c21db94e552b88aa3
Florian Wilhelm <52838694+fwilhe2@users.noreply.github.com>
2025-11-29 10:52:26 +0100

    add playbook to disable snap (#149)

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,4 +11,5 @@ curl --location https://github.com/fwilhe2/system-automation/archive/main.zip -o
 unzip ${tmp_dir}/sysauto.zip -d ${tmp_dir}/extract
 ansible-galaxy install -r ${tmp_dir}/extract/system-automation-main/requirements.yml
 ansible-playbook --ask-become-pass --inventory ${tmp_dir}/extract/system-automation-main/inventory ${tmp_dir}/extract/system-automation-main/epel.yml
+ansible-playbook --ask-become-pass --inventory ${tmp_dir}/extract/system-automation-main/inventory ${tmp_dir}/extract/system-automation-main/snap.yml
 ansible-playbook --ask-become-pass --inventory ${tmp_dir}/extract/system-automation-main/inventory ${tmp_dir}/extract/system-automation-main/common.yml
