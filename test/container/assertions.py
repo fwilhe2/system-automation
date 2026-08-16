80404e05895d85bbe4b82d3d8b10770454ce3a77
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2026-08-16 11:04:49 +0200

    assert_system_properties

--- test/container/assertions.py
+++ test/container/assertions.py
@@ -46,8 +46,11 @@ def assert_system_properties():
         "javac": "-version",
         "mvn": "-version",
         "go": "version",
+        "cargo": "--version",
+        "rustc": "--version",
         "keepassxc-cli": "-version",
         "gcl": "--version",
+        "uv": "--version",
     }
 
     for binary, version_argument in expected_binaries.items():
